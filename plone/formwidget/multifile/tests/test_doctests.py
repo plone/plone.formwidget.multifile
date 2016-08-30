577c00ca99c7f4232b777f216208d9671f451a49
Tom Gross <itconsense@gmail.com>
2016-08-30 20:06:33 +0200

    install coverage in buildout and switch to Python doctest

--- plone/formwidget/multifile/tests/test_doctests.py
+++ plone/formwidget/multifile/tests/test_doctests.py
@@ -1,12 +1,11 @@
 import doctest
 import unittest
 import zope.app.testing.placelesssetup
-import zope.testing.doctest
 
 
 def test_suite():
     return unittest.TestSuite((
-        zope.testing.doctest.DocFileSuite('../README.txt',
+        doctest.DocFileSuite('../README.txt',
             setUp=zope.app.testing.placelesssetup.setUp,
             tearDown=zope.app.testing.placelesssetup.tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
