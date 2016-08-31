e5ae8b48ab76bfe9a0f909ffe207496f60d74f8a
Gil Forcada <gforcada@gnome.org>
2016-08-31 13:01:11 +0200

    Preparing release 2.0
    [ci-skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.2 (unreleased)
+2.0 (2016-08-31)
 ----------------
 
 Incompatibilities:
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.2.dev0'
+version = '2.0'
 
 tests_require = [
     'zope.app.testing',
